d209cc436cbcf4159165956d759ce8299f70b8dc
Keegank <KeeganKavanagh@live.com>
2019-09-11 16:28:42 -0700

    bottle instead of flask

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 import json
-from bottle import HTTPResponse
-from flask import Flask, render_template
+from bottle import route, run, template
 
-@app.route("/")
-def main():
-    return render_template('MilenaTea.html')
 
-if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+@route('/')
+def index():
+    return template('MilenaTea.html')
+
+run(host='localhost', port=8080)
