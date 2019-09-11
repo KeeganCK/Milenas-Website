78534358b7871603d16c81c7ac6abb1e462c0732
Keegank <KeeganKavanagh@live.com>
2019-09-11 16:50:01 -0700

    fixed server

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import json
-from bottle import route, run, template
+import bottle
+import os
 
 
-@route('/')
+@bottle.route('/')
 def index():
     return Bottle.static_file('MilenaTea.html', root="../static/")
+
+application = bottle.default_app()
 
 if __name__ == '__main__':
     bottle.run(
