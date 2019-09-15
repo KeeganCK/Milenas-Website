5f98db61a51f50b678db3b762ed4e61ebe7e9dc2
Keegank <KeeganKavanagh@live.com>
2019-09-14 23:12:21 -0700

    Have a local server

---
+++ app.js
@@ -0,0 +1,35 @@
+let express = require('express');
+
+let app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+
+app.get('/', function(req, res){
+    res.render("MilenaTea")
+});
+
+app.get("/tea", function(req, res){
+    res.render("tea")
+});
+
+app.get("/food", function(req, res) {
+    res.render("food")
+});
+
+
+//Must be last one, or it will catch every route
+app.get("*", function(req, res) {
+    res.send("not a page")
+});
+
+app.listen(port, function() {
+    console.log('Server is running on port:', port)
+});
+
+
+// app.get("r/:subredditName", function(req, res){
+//     res.send("need : for variable")
+// })
