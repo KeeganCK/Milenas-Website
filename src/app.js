533b753056051ef26265e85df9a5933c988e963d
Keegank <KeeganKavanagh@live.com>
2019-09-14 19:53:41 -0700

    Changed server.js to app.js

---
+++ src/app.js
@@ -0,0 +1,16 @@
+let http = require('http');
+let fs = require('fs');
+let index = fs.readFileSync('MilenaTea.html');
+
+const hostname = '127.0.0.1'
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(index);
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
